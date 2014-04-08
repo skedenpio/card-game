0f50161088f084869a9e1106d6c52b342a99b004
danielacucco <danielacucco@gmail.com>
2014-04-08 15:44:18 +0200

    Carta Natale

--- src/it/skedenpio/cards/CardDaniela.java
+++ src/it/skedenpio/cards/CardDaniela.java
@@ -3,7 +3,7 @@ package it.skedenpio.cards;
 public class CardDaniela extends Card {
 
 	public CardDaniela() {
-		super("Natale", "A Natale non siamo tutti più buoni, torna indietro di una casella e guadagni 1 punto");
+		super("Natale", "A  Natale non siamo tutti più buoni, torna indietro di una casella e guadagni 1 punto");
 	}
 
 	@Override
